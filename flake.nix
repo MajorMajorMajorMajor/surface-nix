1da1245ea19e3e16ded22218ee8b58ec3bd86736
Stas <none@localhost>
2026-04-22 01:08:48 -0400

    add github actions kernel build

--- flake.nix
+++ flake.nix
@@ -23,10 +23,12 @@
                     nix.settings.substituters = [
                         "https://cache.nixos.org"
                         "https://linux-surface.cachix.org"
+                        "https://surface-nix.cachix.org"
                     ];
                     nix.settings.trusted-public-keys = [
                         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
                         "linux-surface.cachix.org-1:dorigzlDDV6AacaQLVHHYU8scAzBIlwAhGz/JQ8fVeI="
+                        "surface-nix.cachix.org-1:RsYTWm0eGHpJO6FBL9l/pZMHBYHcI9siaPVNM2oHD+8="
                     ];
 
                     # microsoft-surface.ipts.enable = true;  # touchscreen
